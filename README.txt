c385553e6ef407621edc67e0723dda7272d61033
KanaloaBJJ <ester.tellechea@gmail.com>
2026-09-21 09:49:50 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+BJJ Academy Manager V6 PWA
+Incluye asistencia por sesión (mañana/tarde), 4 grados por cinturón, progresión, avisos, historial, ranking mensual, rachas/preparación para futuras ampliaciones y copias JSON.
+Los umbrales incluidos son DEMOSTRATIVOS y deben configurarse.
+Para instalar como PWA: publicar estos archivos en HTTPS y abrirlos desde el móvil; usar Instalar/Añadir a pantalla de inicio.
---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="bjj-academy-v6";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}).catch(()=>caches.match("./")))));
