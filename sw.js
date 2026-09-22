d5fec20897ca7af47409f270c5fa289ef8f13d02
KanaloaBJJ <ester.tellechea@gmail.com>
2026-09-22 19:43:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE_NAME='kanaloa-bjj-v10-3';
+const CACHE_NAME='kanaloa-bjj-v10-4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./supabase-config.js','./icon.svg'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(
-    fetch(e.request,{cache:'no-store'})
-      .then(r=>{
-        const copy=r.clone();
-        caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
-        return r;
-      })
-      .catch(()=>caches.match(e.request))
-  );
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
