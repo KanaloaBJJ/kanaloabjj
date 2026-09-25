b7424acca5bb620425b171bde83cb9391188b036
KanaloaBJJ <ester.tellechea@gmail.com>
2026-09-25 11:02:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kanaloa-bjj-v11-1';
+const CACHE_NAME='kanaloa-bjj-v11-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./supabase-config.js','./kanaloa-logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
